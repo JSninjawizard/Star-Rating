07e8faf92b92a348c013a776a5057ee49767ffbe
Noam Sokolove <62836489+JSninjawizard@users.noreply.github.com>
2025-07-03 17:59:07 +0300

    working - StarWithinBox

--- app.js
+++ app.js
@@ -1,95 +1,77 @@
-const stars = document.querySelectorAll('.star-container__star')
-const textContainer = document.querySelector('.text-container')
-const starContainer = document.querySelector('.star-container')
+const main = document.querySelector("main");
+const boxes = Array.from(document.querySelectorAll(".box-container"));
+const stars = Array.from(document.querySelectorAll(".star"));
+const textRatings = Array.from(
+  document.querySelectorAll(".text-container__text")
+);
 
+const handleMouseLeaveMain = (e) => {
+  const allBoxes = e.target.children;
+  Array.from(allBoxes).forEach((el) => {
+    el.classList.remove("colored");
+  });
 
+  Array.from(stars).forEach((el) => {
+    el.classList.remove("colored-star");
+  });
+};
 
-const mouseEnter = (e) => {
-    // !with stars
-    const indexOfStar = Array.from(stars).indexOf(e.target)+1
-    // console.log(`Mouse entered ${indexOfStar} star out of ${stars.length}`);
-    
-    // !with e.target --> (Returns HTML COLLECTION and NOT ARRAY!!)
-    // console.log(e.target.parentElement.children); 
+// ! Boxes
+const handleMouseEnterBox = (e) => {
+  const currentBox = e.target;
+  //   currentBox.classList.add("colored");
+  const currentStar = e.target.children[0];
+  currentStar.classList.add("colored-star");
+  const currentBoxIndex = boxes.indexOf(currentBox);
+  const previousBoxesArr =
+    currentBoxIndex !== -1 ? boxes.slice(0, currentBoxIndex) : [];
+  //   console.log(previousBoxesArr);
+  for (let el of previousBoxesArr) {
+    // el.classList.add("colored");
 
-    e.target.classList.add('colored')
+    previousStars = Array.from(el.children);
+    previousStars.forEach((star) => {
+      star.classList.add("colored-star");
+    });
+  }
+
+  // text
+  for (const [index, text] of Object.entries(textRatings)) {
+    if (currentBoxIndex === Number(index)) {
+      text.classList.remove("remove-text");
+      text.classList.add("show-text");
+    }
+  }
+};
+
+const handleMouseLeaveBox = (e) => {
+  const currentBox = e.target;
+  const currentBoxIndex = boxes.indexOf(currentBox);
+  const leftBorder = e.target.getBoundingClientRect().left;
+  const posX = e.clientX;
+  const starsInBoxes = e.target.children[0];
+
+  if (posX <= leftBorder) {
+    starsInBoxes.classList.remove("colored-star");
+  }
+
+  for (const [index, text] of Object.entries(textRatings)) {
+    if (currentBoxIndex === Number(index)) {
+      text.classList.remove("show-text");
+      text.classList.add("remove-text");
+    }
+  }
+};
+
+main.addEventListener("mouseleave", handleMouseLeaveMain);
+for (const box of Array.from(boxes)) {
+  box.addEventListener("mouseenter", handleMouseEnterBox);
+  box.addEventListener("mouseleave", handleMouseLeaveBox);
 }
 
-const mouseLeave = (e) => {
-    const indexOfStar = Array.from(stars).indexOf(e.target)+1
-    // console.log(`Mouse left ${indexOfStar} star out of ${stars.length}`);
-    
-    // e.target.classList.remove('colored')
-    console.log(e.target.parentElement);
-    
-}
-
-
-// stars.forEach((star, i) => {
-//     star.addEventListener('mouseenter', mouseEnter)
-//     star.addEventListener('mouseleave', mouseLeave)
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const handleEnter = (pE) => {
-    console.log('enter');
-    // console.log(e.target.children);
-    const stars = Array.from(pE.target.children)
-
-
-    stars.forEach((el, i, arr) => {
-        console.log(arr);
-        el.addEventListener('mouseenter', (e) => {
-            // console.log(`star #${i+1} out of ${stars.length}`);
-            el.classList.add('colored')
-            
-           const texts = Array.from(textContainer.children)
-           texts.forEach((text, ind) => {
-            if(ind === i) {
-                text.style.display = 'block'
-                text.classList.add('show-text')
-            } else {
-                text.style.display = 'none'
-            }
-
-           })
-        })
-    })
-}
-
-const handleLeave = (pE) => {
-    console.log('left');
-    const stars = Array.from(pE.target.children)
-    stars.forEach((el)=> {
-        if(el.classList.contains('colored')) {
-            el.classList.remove('colored')
-        }
-    })
-}
-
-starContainer.addEventListener('mouseenter', handleEnter)
-starContainer.addEventListener('mouseleave', handleLeave)
+// const testArr = []
+// const test = Array.from(main.children).forEach((el) => {
+//   const eachStar = el.children[0]
+//   testArr.push(eachStar)
+// });
+// console.log(testArr);
